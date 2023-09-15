6b1034aea2ca8318a1d40002a7fe6ba1550c138a
Raniel Haendchen Da Silva <raniel.silva@proway.treina>
2023-09-15 14:11:20 -0300

    Checar Idade pronto

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,4 +1,4 @@
 document.write ("<footer>")
-document.write ("<p>&copy; 2023 Raniel. Exercicios</p>")
-document.write ("<a href='sobre.html'>Sobre</a>")
+document.write ("<p>&copy; 2023 Raniel. Exercicios!</p>")
+document.write ("<a class='sobre' href='sobre.html'>Sobre</a>")
 document.write ("</footer>")
