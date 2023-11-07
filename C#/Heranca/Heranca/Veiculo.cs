54be0b31ea4284ddff60060067d0bdbd34228621
Raniel Haendchen Da Silva <raniel.silva@proway.treina>
2023-11-07 17:10:41 -0300

    Adicionado pasta de heranca. Explicação do professor

---
+++ C#/Heranca/Heranca/Veiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    class Veiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public void ronco()
+        {
+            Console.WriteLine("Ronco do veiculo");
+        }
+    }
+}
